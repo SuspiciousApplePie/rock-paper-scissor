50a7e87575838940f166f133d3b0bf3c9e74d75c
Shishi <andreiusita6@gmail.com>
2025-07-20 23:15:02 +0800

    fix: Re order function placement

--- javascript.js
+++ javascript.js
@@ -36,33 +36,20 @@ function handleComputerWin (computerChoice, humanChoice, computerScore) {
     return computerScore + 1;
 }
 
+// runs if game is tied
+function declareTie () {
+    content.textContent = 'Tie';
+    body.appendChild(content);
+}
+
+// Displays score
 function displayScore (humanTotalScore, computerTotalScore) {
     score.textContent = `Computer: ${computerTotalScore} Human: ${humanTotalScore}`;
     body.appendChild(score);
 }
 
-// Playing function
-function playRound(humanChoice, computerChoice) {
-    if (gameOver) return;
-
-    // IF rock against scissors, rock wins
-    if (humanChoice === 'rock' && computerChoice === 'scissors') {
-        humanTotalScore = addHumanScore(humanChoice, computerChoice, humanTotalScore);
-    // ELSEIF paper against rock, paper wins
-    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-        humanTotalScore = addHumanScore(humanChoice, computerChoice, humanTotalScore);
-    // ELSEIF scissors against paper, scissors wins
-    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-        humanTotalScore = addHumanScore(humanChoice, computerChoice, humanTotalScore);
-    // ELSEIF choice are similar
-    } else if (humanChoice === computerChoice) {
-        declareTie();
-    // ELSE player lose
-    } else {
-        computerTotalScore = addComputerScore(computerChoice, humanChoice, computerTotalScore); 
-    }
-    displayScore(humanTotalScore, computerTotalScore);
-
+// Check who wins
+function checkWinner(humanTotalScore, computerTotalScore) {
     if (humanTotalScore === 5) {
         gameOver = true;
         winner.textContent = 'Human Wins!'
